af52b8936e5578d20f7e4c3317f86283e33f17d5
tirth-2001 <tirthgpatel.27@gmail.com>
2022-06-18 10:31:21 +0530

    redis removed

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import express from 'express'
 import axios from 'axios'
-import { createClient } from 'redis'
+// import { createClient } from 'redis'
 import { rootHandler, helloHandler } from './handlers'
 
-const redisClient = createClient()
+// const redisClient = createClient()
 
-redisClient.connect()
+// redisClient.connect()
 
 const app = express()
 const port = process.env.PORT || 8080
@@ -13,22 +13,22 @@ const port = process.env.PORT || 8080
 app.get('/', rootHandler)
 app.get('/hello/:name', helloHandler)
 
-app.get('/photos', async (req, res) => {
-	const response = await redisClient.get('photos')
-	const photos = response ? JSON.parse(response) : null
+// app.get('/photos', async (req, res) => {
+// 	const response = await redisClient.get('photos')
+// 	const photos = response ? JSON.parse(response) : null
 
-	if (!!photos.length) {
-		console.log('Found in cache', photos)
+// 	if (!!photos.length) {
+// 		console.log('Found in cache', photos)
 
-		return res.json(JSON.parse(response))
-	} else {
-		const { data } = await axios.get(
-			`https://jsonplaceholder.typicode.com/photos`
-		)
-		await redisClient.set('photos', JSON.stringify(data))
-		res.json(data)
-	}
-})
+// 		return res.json(JSON.parse(response))
+// 	} else {
+// 		const { data } = await axios.get(
+// 			`https://jsonplaceholder.typicode.com/photos`
+// 		)
+// 		await redisClient.set('photos', JSON.stringify(data))
+// 		res.json(data)
+// 	}
+// })
 
 app.get('/photos/:id', async (req, res) => {
 	const { id } = req.params

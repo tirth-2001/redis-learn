0430197585102e1e2e1988851414a3bc940a54df
tirth-2001 <tirthgpatel.27@gmail.com>
2022-06-18 17:50:02 +0530

    response chnaged

--- src/handlers.ts
+++ src/handlers.ts
@@ -9,7 +9,7 @@ type HelloBuilder = (name: string) => HelloResponse
 const helloBuilder: HelloBuilder = (name) => ({ hello: name })
 
 export const rootHandler = (_req: Request, res: Response) => {
-	return res.send('API is working 🤓')
+	return res.send('API is working fine! 🤓')
 }
 
 export const helloHandler = (req: Request, res: Response) => {

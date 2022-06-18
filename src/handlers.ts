3849d4e86319b41a188f356a90d0f91fa6ad8446
tirth-2001 <tirthgpatel.27@gmail.com>
2022-06-18 22:23:35 +0530

    fine 1

--- src/handlers.ts
+++ src/handlers.ts
@@ -9,7 +9,7 @@ type HelloBuilder = (name: string) => HelloResponse
 const helloBuilder: HelloBuilder = (name) => ({ hello: name })
 
 export const rootHandler = (_req: Request, res: Response) => {
-	return res.send('API is running fine! 🤓')
+	return res.send('API is running fine 1! 🤓')
 }
 
 export const helloHandler = (req: Request, res: Response) => {
